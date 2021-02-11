25bc6e38d95fffdb65ec86627784bf312812357e
saifultech <saifultech.official@gmail.com>
2021-02-11 21:51:49 +0800

    done

--- PASTYEARJAVA/CSC435DECEMBER2019/FoodFestival.java
+++ PASTYEARJAVA/CSC435DECEMBER2019/FoodFestival.java
@@ -1,7 +1,54 @@
-package PASTYEARJAVA.CSC435DECEMBER2019;
+import java.io.*;  
+
+/**
+ * @author SAIFULTECH
+ * 
+ */
+
 
 public class FoodFestival {
     public static void main(String args[]){
-        System.out.println("Test");
+       try {
+           BufferedReader in = new BufferedReader(new FileReader("food.txt"));
+           FileWriter outWestern = new FileWriter("Western.txt");
+           FileWriter outMalay = new FileWriter("Malay.txt");
+           FileWriter outchinesse = new FileWriter("chinesse.txt");
+            
+           outWestern.write("**************Western Food****************"+"\n");
+           outMalay.write("**************Malay Food****************"+"\n");
+           outchinesse.write("**************Chinesse Food****************"+"\n");
+           outWestern.write("Name of food\tPrice\tChef"+"\n");
+           outMalay.write("Name of food\tPrice\tChef"+"\n");
+           outchinesse.write("Name of food\tPrice\tChef"+"\n");
+           String line;
+           int Zack=0;
+           while((line=in.readLine())!=null){
+            String[] array = line.split(";",4);
+            double d = Double.parseDouble(array[2]);
+            if(d>20)
+            System.out.println(array[0]+"\t"+array[2]);
+            if(array[3].equals("Zack"))
+            Zack=Zack+1;
+            if(array[1].equals("Western")){
+            outWestern.write(WriteMenu(line)+"\n");
+            }else if(array[1].equals("Malay")){
+            outMalay.write(WriteMenu(line)+"\n");
+            }else{
+            outchinesse.write(WriteMenu(line)+"\n");
+            }   
+           }
+           in.close();
+           outWestern.close();
+           outMalay.close();
+           outchinesse.close();
+           System.out.println("Number food cook by Zack are "+Zack);
+       } catch (Exception e) {
+        System.out.println("Something went wrong.");
+       }
+      
+    }
+    public static String WriteMenu(String line){
+    String[] array = line.split(";",4);
+    return array[0]+"\t"+array[2]+"\t"+array[3];
     }
 }
